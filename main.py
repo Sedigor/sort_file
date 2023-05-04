9e954a29bd26e6a5dc02918067606a3ba49b83ba
Ihor Siedielnikov <sisemail92@gmail.com>
2023-05-05 01:11:06 +0300

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+for arg in sys.argv:
+    print(arg)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.INFO)
+    path_to_folder = path
+    main(path_to_folder)
---
+++ sorty.py
@@ -0,0 +1,34 @@
+import sys
+
+
+# def get_folder_path():
+#     path = ""
+#     for arg in sys.argv:
+#         path += arg
+#     return path
+
+# print("the path is: {path}")
+
+for arg in sys.argv:
+    print(arg)
+
+# def create_folders(folder_dict):
+#     for folder in folder_dict.keys():
+#         if not os.path.exists("{}\\{}".format(path, folder))
+
+
+# def main(path):
+#     try:
+#         extensions = {
+#             "images": ('JPEG', 'PNG', 'JPG', 'SVG'),
+#             "documents": ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
+#             "audio": ('MP3', 'OGG', 'WAV', 'AMR'),
+#             "video": ('AVI', 'MP4', 'MOV', 'MKV'),
+#             "archives": ('ZIP', 'GZ', 'TAR')
+#         }
+        
+#         with open
+
+
+    
+    
